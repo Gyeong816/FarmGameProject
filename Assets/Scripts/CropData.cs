799b7648807bf29e6c3bfc2261dc1ef3ad786b24
Gyeong Jeong <wjdrud7180@gmail.com>
2025-06-09 20:10:00 +0900

    작물 데이타 스크립터블 오브젝트 정의

---
+++ Assets/Scripts/CropData.cs
@@ -0,0 +1,12 @@
+// SimpleCropData.cs
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CropData", menuName = "ScriptableObjects/CropData")]
+public class SimpleCropData : ScriptableObject
+{
+    [Tooltip("0일→1단계, 1일→2단계, 2일→3단계(수확), 3일→4단계(시들음)")]
+    public GameObject[] stagePrefabs = new GameObject[5];
+    
+    [Tooltip("수확 시 나올 아이템 프리팹")]
+    public GameObject harvestPrefab;
+}
